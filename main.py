6428181f8c6ca93350366591ddbf6d904c30c232
chrstnhnry <chrstnhnry@gmail.com>
2022-02-21 21:21:37 -0600

    Added APINinja Feature

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import gmplot
+import requests
 
 # Create the map plotter:
 apikey = 'AIzaSyCT00gibgn-VBeOAoDjIEnSSq5LlRMpOEs' # (your API key here)
 gmap = gmplot.GoogleMapPlotter(0, 0, 2, apikey=apikey)
+
+#get lat, lon of IP
+ip_addr = '73.9.149.180'
+api_url = 'https://api.api-ninjas.com/v1/iplookup?address={}'.format(ip_addr)
+response = requests.get(api_url, headers={'X-Api-Key': 'gUMd15sgzurPzg2Smfr1rw==kchzmYCyqNNqGklW'})
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error:", response.status_code, response.text)
+
 
 # Mark a hidden gem:
 gmap.marker(37.770776, -122.461689, color='cornflowerblue')
