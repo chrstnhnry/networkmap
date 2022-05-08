9a612ccb9143a509c208ab902cc85f564ba338ee
chrstnhnry <chrstnhnry@gmail.com>
2022-05-08 17:47:41 -0500

    Add csv reader

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import gmplot
 import requests
 import os
+import csv
 
 # Create the map plotter:
-apikey = 'AIzaSyCT00gibgn-VBeOAoDjIEnSSq5LlRMpOEs' # (your API key here)
+apikey = 'AIzaSyDil4ygmmAbn8L2qit_AOivek7nIa82Ozs' # (your API key here)
 gmap = gmplot.GoogleMapPlotter(0, 0, 2, apikey=apikey)
 
 # Get lat, lon of IP
@@ -15,13 +16,46 @@ gmap = gmplot.GoogleMapPlotter(0, 0, 2, apikey=apikey)
 ##else:
 ##    print("Error:", response.status_code, response.text)
 
+
+mal = []
+saf = []
+
 # Run netstat
-output_command = os.popen("netstat -nb").readlines()
-output_command = output_command[1:20]
-lst = [e[44:67] for e in output_command]
-print(lst)
+def netstat():
+    netstat = os.popen("netstat -nb").readlines()
+    return onlyIP(netstat)
+
+#Collect only public IP from Netstat
+def onlyIP(output):
+    output = output[1:20]
+    lst = [e[44:67] for e in output]
+    return lst
+    
+    
 # Mark a hidden gem:
-gmap.marker(37.770776, -122.461689, color='cornflowerblue')
+gmap.marker(32.5232, -92.6379, color='cornflowerblue')
 
 # Draw the map to an HTML file:
 gmap.draw('map.html')
+
+# Read csv file
+with open('addresses.csv', mode ='r')as file:
+   
+  # reading the CSV file
+  csvFile = csv.reader(file)
+ 
+  # displaying the contents of the CSV file
+  for lines in csvFile:
+        print(lines)
+        if(lines[1] == 'M'):
+            mal.append(lines[0])
+        else:
+            saf.append(lines[0])
+
+print(mal)
+
+
+
+
+
+#print(netstat())
